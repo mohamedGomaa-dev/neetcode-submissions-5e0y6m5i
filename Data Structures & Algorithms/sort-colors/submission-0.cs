df32cf40e8792c08ea9fa9cad79cbe1b3b2c7fd7
mohamedGomaa-dev <mohammedgomaa342@gmail.com>
2026-05-07 10:08:53 +0300

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public void SortColors(int[] nums) {
+        for (int i = 1; i < nums.Length; i++) {
+            int j = i - 1;
+
+            while (j >= 0 && nums[j + 1] < nums[j]) 
+            {
+                int temp = nums[j + 1];
+                nums[j+1] = nums[j];
+                nums[j] = temp;
+                j--;
+            }
+        }
+    }
+
+    
+}
