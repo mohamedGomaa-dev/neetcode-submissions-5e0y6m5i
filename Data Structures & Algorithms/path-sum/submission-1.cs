70aba337ab33bd19a0275adc774c54a4eb77a486
mohamedGomaa-dev <mohammedgomaa342@gmail.com>
2026-05-07 09:53:46 +0300

    Add: path-sum - submission-1

---
+++ Data Structures & Algorithms/path-sum/submission-1.cs
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool HasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+
+        targetSum -= root.val;
+        if (root.left == null && root.right == null) {
+            return (targetSum == 0);
+        }
+        if (HasPathSum(root.left, targetSum)) {
+            return true;
+        }
+        if (HasPathSum(root.right, targetSum)) {
+            return true;
+        }
+        targetSum += root.val;
+        return false;
+    }
+}
