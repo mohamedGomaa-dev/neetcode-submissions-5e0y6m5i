0a68e8704e2d04f72d74497c071d0d576bc3d6b5
mohamedGomaa-dev <mohammedgomaa342@gmail.com>
2026-06-21 07:48:07 +0300

    Add: concatenation-of-array - submission-2

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-2.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] GetConcatenation(int[] nums)
+    {
+        int[] result = new int[nums.Length * 2];
+        for (int i = 0; i < nums.Length;i++)
+        {
+            result[i] = nums[i];
+            result[nums.Length + i] = nums[i];
+        }
+        return result;
+    }
+}
