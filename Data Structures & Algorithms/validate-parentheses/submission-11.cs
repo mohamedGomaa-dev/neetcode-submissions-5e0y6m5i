71902330b15069f750c36307d152db308c4c8952
mohamedGomaa-dev <mohammedgomaa342@gmail.com>
2026-07-19 13:55:15 +0300

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public bool IsValid(string s) {
+        Stack<char> stack = new Stack<char>();
+        for (int i = 0; i < s.Length; i++) {
+            char c = s[i];
+            if (c == '(' || c == '{' || c == '[') {
+                stack.Push(c);
+            } else {
+                if (stack.Count() == 0) return false;
+                char top = (char)stack.Pop();
+                if ((c == ')' && top != '(') || (c == ')' && top != '(') || (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.Count() == 0;
+    }
+}
